f8f6d6b9ddbb6fb96cc8c45be1fbc1ff4678a2ef
jellyjo <juicy_joji@yu.ac.kr>
2024-01-12 18:50:14 +0900

    vue componenet and props

---
+++ src/components/Container.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+      Container
+      <!-- {{ list }} -->
+      <!-- <Post v-bind:list="list[0]"/>
+      <Post v-bind:list="list[1]"/>
+      <Post v-bind:list="list[2]"/> -->
+      <Post v-bind:list="list[i]" v-for="(a,i) in list" :key="i" />
+  </div>
+</template>
+
+<script>
+import Post from '../components/Post.vue'
+
+export default {
+    name:'Container',
+    components:{Post},
+    props:{list:Array}
+
+}
+</script>
+
+<style>
+
+</style>
